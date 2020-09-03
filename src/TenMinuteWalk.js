fc300ba753a0097d7169107338918c24cc1a374f
Tim <TimCPB@github.com>
2020-09-03 13:03:32 +0100

    refactor with private function _resetDirections

--- src/TenMinuteWalk.js
+++ src/TenMinuteWalk.js
@@ -1,10 +1,26 @@
-function isTenMinuteWalk(array) {
-  var north = 0
-  var south = 0
-  var east = 0
-  var west = 0
+var north = 0
+var south = 0
+var east = 0
+var west = 0
 
-  array.forEach(function (direction) {
+function isTenMinuteWalk(array) {
+  _parseDirections(array)
+  console.log(`north = ${north}`)
+  console.log(`south = ${south}`)
+  console.log(`east = ${east}`)
+  console.log(`west = ${west}`)
+  if(array.length === 10 && north === south && east === west){
+    _resetDirections()
+    return true
+  }
+  else
+    _resetDirections()
+    return false
+  
+}
+
+function _parseDirections(directionsArray) {
+  directionsArray.forEach(function (direction) {
     if(direction === 'n'){
       north++
       }
@@ -17,10 +33,11 @@ function isTenMinuteWalk(array) {
     else
       west++
       })
-    if(array.length === 10 && north === south && east === west){
-      return true
-    }
-    else
-     return false
-   
+}
+
+function _resetDirections(){
+  north = 0
+  south = 0
+  east = 0
+  west = 0
 }
